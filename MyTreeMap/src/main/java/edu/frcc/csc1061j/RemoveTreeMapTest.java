157abc22f1acd2c7a17accc6fd2ae63e1863d2f9
Kishore Menezes <kishore.menezes@frontrange.edu>
2023-11-25 10:00:11 -0700

    Committed test file for Node removal.

---
+++ MyTreeMap/src/main/java/edu/frcc/csc1061j/RemoveTreeMapTest.java
@@ -0,0 +1,56 @@
+package edu.frcc.csc1061j;
+
+import java.util.Map;
+
+
+public class RemoveTreeMapTest {
+
+	public static void main(String[] args) {
+		MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+		map.put(8, 8);
+		map.put(12, 12);
+		map.put(4, 4);
+		map.put(14, 14);
+		map.put(10, 10);
+		map.put(6, 6);
+		map.put(2, 2);
+		map.put(1, 1);
+		map.put(3, 3);
+		map.put(7, 7);
+		map.put(5, 5);
+		map.put(11, 11);
+		map.put(9, 9);
+		map.put(13, 13);
+		map.put(15, 15);
+
+		printInOrder(map);
+		
+		int removedKey = map.remove(5);
+		System.out.println("Removed key: " + removedKey);
+		printInOrder(map);
+		
+		removedKey = map.remove(6);
+		System.out.println("Removed key: " + removedKey);
+		printInOrder(map);
+		
+		removedKey = map.remove(3);
+		System.out.println("Removed key: " + removedKey);
+		printInOrder(map);
+		
+		removedKey = map.remove(4);
+		System.out.println("Removed key: " + removedKey);
+		printInOrder(map);
+		
+		removedKey = map.remove(8);
+		System.out.println("Removed value: " + removedKey);	
+		printInOrder(map);
+		
+	}
+
+	private static void printInOrder(MyTreeMap<Integer, Integer> map) {
+		for(Integer value: map) {
+			System.out.print(value.toString() + " ");
+		}
+		System.out.println();
+	}
+}
